30319c01382db726e3a89bcf5d4293971e84a8ff
1783440856@qq.com <zxcvbnm000++>
2022-04-20 23:14:06 +0800

    Initial commit

---
+++ src/com/jtx/delo1.java
@@ -0,0 +1,11 @@
+package com.jtx;
+
+/**
+ * jiataoxiang
+ * Date:2022/4/2023:13
+ **/
+public class delo1 {
+    public static void main(String[] args) {
+        System.out.println("zheshiwode ");
+    }
+}
